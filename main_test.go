dc8f6ea199bfa420bac981df149e6dfd029bca06
Ngan Nguyen <ngann.dev@gmail.com>
2019-04-02 14:49:40 -0700

    use table driven tests

--- main_test.go
+++ main_test.go
@@ -51,25 +51,23 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got , want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want float64
+	} {
+		{Rectangle{12,6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
 	}
 
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{ 12, 6}
-		checkArea(t , rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got , tt.want)
+		}
+	}
 }
-//checkArea is a helper function which is asking for a Shape to be passed
-// The code will not compile unless a shape is being passed, to do this we will need to use an interface declaration "type Shape interface { Area() float 64}"
-//Errorf function lets us use formatting features to create descriptive error messages.
+
+//removed the helper and created an anonymous struct. areaTests is a struct array with two feilds shape and want.
+// Below the struct are the array filled with cases for rec, circ, triangle.. etc..
+//
