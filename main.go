4d44a73f009efe03ac856f696e022644e959cf67
Ngan Nguyen <ngann.dev@gmail.com>
2019-04-03 11:28:44 -0700

    wrote test for single row dataset

--- main.go
+++ main.go
@@ -1,85 +1,114 @@
 package main
 
 import (
-	// "encoding/json"
-	// "io/ioutil"
-	"log"
-	// "net/http"
+	"fmt"
 	"regexp"
-	// "strconv"
+	"strconv"
 	"strings"
-	// "fmt"
-	// "reflect"
-	"math"
 )
+
+var sampleRow = `
+	2019 04 02 17 00  1.7  1.7 16.0  0.2  3.4 WNW  SW      SWELL  9.6 292
+`
+
+// #YY  MM DD hh mm WVHT  SwH  SwP  WWH  WWP SwD WWD  STEEPNESS  APD MWD
 
 type SurfData struct {
 	Year, Month, Day, Hour, Min int
 	WVHT, SwH, SwP, WWH, WWP    float64
 	SwD, WWD, STEEPNESS         string
 	APD, MWD                    float64
-	Count                       int
 }
 
 func main() {
-	// http.HandleFunc("/", surfDataRequest)
-	// http.ListenAndServe(":3000", nil)
+	// k := removeSpacesFromString(sampleRow)
+	m := removeSpaces(sampleRow)
+	// m := parseStringCount(k)
+	// n := parseString(k)
+	o := getRowDataWWD(m)
+	// fmt.Println(sampleString)
+	// fmt.Println(k)
+	fmt.Println(m)
+	fmt.Println(o)
 }
 
-//the httprequest will return a response that is a string
-// fmt.Println(reflect.TypeOf(surf)) is a string
-// fmt.Println(reflect.TypeOf(rows)) is now an array of string because we split the data.
+func removeSpaces(data string) []string {
+	surf := data
+	newString := strings.Fields(surf)
+	return newString
+}
 
-func ParseString(data string) {
-	surf := string(data)
-	//to match all leading/trailing whitespac
+func removeSpacesFromString(data string) string {
+	surf := data
 	leadSpace := regexp.MustCompile(`^[\s\p{Zs}]+|[\s\p{Zs}]+$`)
-	//to match 2 or more whitespace symbols inside a string
 	extraSpace := regexp.MustCompile(`[\s\p{Zs}]{2,}`)
-	final := leadSpace.ReplaceAllString(surf, "")
-	final = extraSpace.ReplaceAllString(final, " ")
-	rows := strings.Split(final, "\n")
-		log.Println(rows)
-	
+	newString := leadSpace.ReplaceAllString(surf, "")
+	newString = extraSpace.ReplaceAllString(newString, " ")
+
+	return newString
 }
 
-type Rectangle struct {
-	Width float64
-	Height float64
+func parseStringCount(data string) int {
+	row := strings.Split(data, " ")
+	return len(row)
 }
 
-type Circle struct {
-	Radius float64
+func parseString(data string) []string {
+	row := strings.Split(data, " ")
+	return row
 }
 
-type Triangle struct {
-	Base float64
-	Height float64
+func getRowDataWWD(data []string) string {
+	WWD := string(data[11])
+	return WWD
 }
 
-
-type Shape interface {
-	Area() float64
+func getRowDataSwH(data []string) float64 {
+	SwH, _ := strconv.ParseFloat(data[6], 64)
+	return SwH
+}
+func getRowDataSwP(data []string) float64 {
+	SwP, _ := strconv.ParseFloat(data[7], 64)
+	return SwP
 }
 
-// Go interface resolution is implicit , If the type you pass in matches what the interface is asking for, it will compile.
-// Rectangle has a method called Area that returns a float64 so it satisfies the Shape interface
-// Circle has a method called Area that returns a float64 so it satisfies the Shape interface
-
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func windScore(data string) float64 {
+	if data == "E" {
+		return 5
+	} else if data == "NE" {
+		return 4
+	} else if data == "SE" {
+		return 4
+	} else if data == "S" {
+		return 3
+	} else {
+		return 1
+	}
 }
 
-func (r Rectangle) Area() float64 {
-	return r.Width * r.Height
+func swellScore(data float64) float64 {
+	if data >= 16 {
+		return 5
+	} else if data >= 12 {
+		return 4
+	} else if data >= 10 {
+		return 3
+	} else {
+		return 1
+	}
 }
 
-func (c Circle) Area() float64 {
-	return math.Pi * c.Radius * c.Radius
+func waveScore(swp, swh float64) float64 {
+	wave := swp * swh
+	if wave > 30 {
+		return 5
+	} else if wave >= 25 {
+		return 4
+	} else if wave >= 20 {
+		return 3
+	} else if wave >= 11 {
+		return 2
+	} else {
+		return 1
+	}
 }
-
-func (t Triangle) Area() float64 {
-	return (t.Base * t.Height) *0.5
-}
-
-//type Shape interface has Area() method which then allows each shape to call it's own method.
--- main_test.go
+++ main_test.go
@@ -2,78 +2,96 @@ package main
 
 import (
 	"testing"
-	"regexp"
-	"strings"
-	"reflect"
 )
 
 var testRow = `
 	2019 04 02 17 00  1.7  1.7 16.0  0.2  3.4 WNW  SW      SWELL  9.6 292
 `
 
-var testString = `
-	#YY  MM DD hh mm WVHT  SwH  SwP  WWH  WWP SwD WWD  STEEPNESS  APD MWD
-	#yr  mo dy hr mn    m    m  sec    m  sec  -  degT     -      sec degT
-	2019 04 02 17 00  1.7  1.7 16.0  0.2  3.4 WNW  SW      SWELL  9.6 292
-	2019 04 02 16 00  1.8  1.8 14.8  0.2  3.4 WNW NNW      SWELL  9.3 286
-	2019 04 02 15 00  1.7  1.7 16.0  0.2  3.4 WNW NNE      SWELL  9.4 288
-	2019 04 02 14 00  1.5  1.5 13.8  0.2  3.6 WNW NNW      SWELL  9.1 285
-	2019 04 02 13 00  1.7  1.7 17.4  0.2  3.8 WNW NNE      SWELL 10.1 288
-	2019 04 02 12 00  1.9  1.8 16.0  0.2  3.7 WNW NNW      SWELL 10.5 289
-`
+var testNewRow = `2019 04 02 17 00 1.7 1.7 16.0 0.2 3.4 WNW SW SWELL 9.6 292`
 
-func TestParseString(t *testing.T) {
-	surf := string(testString)
-	//to match all leading/trailing whitespac
-	leadSpace := regexp.MustCompile(`^[\s\p{Zs}]+|[\s\p{Zs}]+$`)
-	//to match 2 or more whitespace symbols inside a string
-	extraSpace := regexp.MustCompile(`[\s\p{Zs}]{2,}`)
-	final := leadSpace.ReplaceAllString(surf, "")
-	final = extraSpace.ReplaceAllString(final, " ")
-	rows := strings.Split(final, "\n")
-	var dataType []string
-
-	if reflect.TypeOf(rows) !=  reflect.TypeOf(dataType) {
-		t.Errorf("Expected %T, got %T", dataType, rows)
+func TestRemoveSpacesFromString(t *testing.T) {
+	data := testRow
+	got := removeSpacesFromString(data)
+	want := testNewRow
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
 
-
-func TestPerimeter(t *testing.T) {
-    rectangle := Rectangle{10.0, 10.0}
-    got := Perimeter(rectangle)
-    want := 40.0
-
-    if got != want {
-        t.Errorf("got %.2f want %.2f", got, want)
-    }
-}
-
-func TestArea(t *testing.T) {
-
-	areaTests := []struct {
-		name string
-		shape Shape
-		hasArea float64
-	} {
-		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
-        {name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
-        {name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
-	}
-
-	for _, tt := range areaTests {
-		// using tt.name from the case to use it as the `t.Run` test name
-		t.Run(tt.name, func(t *testing.T) {
-			got := tt.shape.Area()
-			if got != tt.hasArea {
-				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
-			}
-		})
+func TestParseStringCount(t *testing.T) {
+	data := testRow
+	newRow := removeSpacesFromString(data)
+	got := parseStringCount(newRow)
+	want := 15
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
 	}
 }
 
-//removed the helper and created an anonymous struct. areaTests is a struct array with two feilds shape and want.
-// Below the struct are the array filled with cases for rec, circ, triangle.. etc..
-// The test array was not descriptive enough and we modified the struct and test array to show what is being passed into the function.
-// removed feild want and replaced with hasArea , add name field
-// 
+func TestGetRowWWD(t *testing.T) {
+	data := testRow
+	newRow := removeSpacesFromString(data)
+	removeRowSpaces := parseString(newRow)
+	got := getRowDataWWD(removeRowSpaces)
+	want := "SW"
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestGetRowSwP(t *testing.T) {
+	data := testRow
+	newRow := removeSpacesFromString(data)
+	removeRowSpaces := parseString(newRow)
+	got := getRowDataSwP(removeRowSpaces)
+	want := 16.0
+	if got != want {
+		t.Errorf("got %f want %f", got, want)
+	}
+}
+
+func TestGetRowSwH(t *testing.T) {
+	data := testRow
+	newRow := removeSpacesFromString(data)
+	removeRowSpaces := parseString(newRow)
+	got := getRowDataSwH(removeRowSpaces)
+	want := 1.7
+	if got != want {
+		t.Errorf("got %f want %f", got, want)
+	}
+}
+
+func TestWindScore(t *testing.T) {
+	data := removeSpaces(testNewRow)
+	wwd := getRowDataWWD(data)
+	got := windScore(wwd)
+	want := 1.0
+
+	if got != want {
+		t.Errorf("got %f want %f", got, want)
+	}
+}
+
+func TestSwellScore(t *testing.T) {
+	data := removeSpaces(testNewRow)
+	swell := getRowDataSwP(data)
+	got := swellScore(swell)
+	want := 5.0
+
+	if got != want {
+		t.Errorf("got %f want %f", got, want)
+	}
+}
+
+func TestWaveScore(t *testing.T) {
+	data := removeSpaces(testNewRow)
+	swp := getRowDataSwP(data)
+	swh := getRowDataSwH(data)
+	got := waveScore(swp, swh)
+	want := 4.0
+
+	if got != want {
+		t.Errorf("got %f want %f", got, want)
+	}
+}
